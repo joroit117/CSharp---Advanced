26cfbfc7dc2bd73a6895d73c782f073d01b0ead7
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-09-18 15:11:31 +0300

    Add files via upload

---
+++ 1. Stacks And Queues/1.ReverseStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strText = Console.ReadLine();
+            Stack<char> sMyStack = new Stack<char>();
+            for (int i = 0; i < strText.Length; i++)
+            {
+                sMyStack.Push(strText[i]);
+            }
+            while(sMyStack.Count() > 0)
+            {
+                Console.Write(sMyStack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 1. Stacks And Queues/2.StackSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strIntegers = Console.ReadLine().Split();
+            Stack<int> myStack = new Stack<int>();
+
+            for(int i = 0; i < strIntegers.Length; i++)
+            {
+               myStack.Push(int.Parse(strIntegers[i]));
+            }
+
+            string[] command = Console.ReadLine().Split();
+
+            while(!string.Equals(command[0], "end", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if(string.Equals(command[0], "add", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    myStack.Push(int.Parse(command[1]));
+                    myStack.Push(int.Parse(command[2]));
+
+                }
+                else if (string.Equals(command[0], "remove", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    if(int.Parse(command[1]) <= myStack.Count)
+                    {
+                        for (int i = 0; i < int.Parse(command[1]); i++)
+                        {
+                            myStack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().Split();
+            }
+            int nSum = 0;
+            int nMyStackSize = myStack.Count;
+            for(int i = 0; i < nMyStackSize; i++)
+            {
+                nSum += myStack.Pop();
+            }
+            Console.WriteLine("Sum: " + nSum);
+        }
+    }
+}
