fe84f0778eeedd3ee142d8e3966c20f9c38b842b
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-10-17 13:32:54 +0300

    Add files via upload

---
+++ 2. StacksAndQueues-Exercise/1.BasicStackOperations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int S = int.Parse(input[1]);
+            int X = int.Parse(input[2]);
+
+            Stack<int> numbers = new Stack<int>();
+            int smallestNum = Int32.MaxValue;
+
+            string[] inputNums = Console.ReadLine().Split();
+
+            for (int i = 0; i < N; i++)
+            {
+                int a = int.Parse(inputNums[i]);
+                numbers.Push(a);
+            }
+            for(int i = 0; i < S; i++)
+            {
+                numbers.Pop();
+            }
+            if(numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                bool isFound = false;
+                while (numbers.Count > 0)
+                {
+                    int temp = numbers.Pop();
+                    if (temp == X)
+                    {
+                        Console.WriteLine("true");
+                        isFound = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (temp < smallestNum) smallestNum = temp;
+                    }
+                }
+                if (!isFound)
+                {
+                    Console.WriteLine(smallestNum);
+                }
+            }
+        }
+    }
+}
---
+++ 2. StacksAndQueues-Exercise/2.BasicQueueOperations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int S = int.Parse(input[1]);
+            int X = int.Parse(input[2]);
+
+            Queue<int> numbers = new Queue<int>();
+            int smallestNum = Int32.MaxValue;
+
+            string[] inputNums = Console.ReadLine().Split();
+
+            for (int i = 0; i < N; i++)
+            {
+                int a = int.Parse(inputNums[i]);
+                numbers.Enqueue(a);
+            }
+            for (int i = 0; i < S; i++)
+            {
+                numbers.Dequeue();
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                bool isFound = false;
+                while (numbers.Count > 0)
+                {
+                    int temp = numbers.Dequeue();
+                    if (temp == X)
+                    {
+                        Console.WriteLine("true");
+                        isFound = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (temp < smallestNum) smallestNum = temp;
+                    }
+                }
+                if (!isFound)
+                {
+                    Console.WriteLine(smallestNum);
+                }
+            }
+        }
+    }
+}
