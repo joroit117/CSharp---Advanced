c572d5b76c65483d5723a11ae9662adcdc2575cf
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-12-02 23:56:01 +0200

    Add files via upload

---
+++ 4. SetsAndDictionaries-Advanced/3.ProductShop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionaries
+{
+    public class Program
+    {
+        static void Main()
+        {
+            SortedDictionary<string, Dictionary<string, double>> supermarket = new SortedDictionary<string, Dictionary<string, double>>();
+            while(true)
+            {
+                string[] input = Console.ReadLine().Split(", ");
+                if (input[0] == "Revision")
+                    break;
+                if(supermarket.ContainsKey(input[0]))
+                {
+                    supermarket[input[0]].Add(input[1], double.Parse(input[2]));
+                }
+                else
+                {
+                    supermarket.Add(input[0], new Dictionary<string, double>());
+                    supermarket[input[0]].Add(input[1], double.Parse(input[2]));
+                }
+            }
+            foreach(var item  in supermarket)
+            {
+                Console.WriteLine(item.Key + "->");
+                foreach(var val in item.Value)
+                {
+                    Console.WriteLine("Product: " + val.Key + ", Price: " + val.Value);
+                }
+            }
+        }
+    }
+}
---
+++ 4. SetsAndDictionaries-Advanced/4.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionaries
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Dictionary<string, List<string>>> ccc = new Dictionary<string, Dictionary<string, List<string>>>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                if(ccc.ContainsKey(input[0]))
+                {
+                    if(ccc[input[0]].ContainsKey(input[1]))
+                    {
+                        ccc[input[0]][input[1]].Add(input[2]);
+                    }
+                    else
+                    {
+                        ccc[input[0]].Add(input[1], new List<string>());
+                        ccc[input[0]][input[1]].Add(input[2]);
+                    }
+                }
+                else
+                {
+                    ccc.Add(input[0], new Dictionary<string, List<string>>());
+                    ccc[input[0]].Add(input[1], new List<string>());
+                    ccc[input[0]][input[1]].Add(input[2]);
+                }
+            }
+            foreach(var c in ccc)
+            {
+                Console.WriteLine(c.Key + ":");
+                foreach(var cc in c.Value)
+                {
+                    Console.Write(cc.Key + " -> ");
+                    for(int i = 0; i < cc.Value.Count; i++)
+                    {
+                        if (i == cc.Value.Count - 1)
+                        {
+                            Console.WriteLine(cc.Value[i]);
+                        }
+                        else
+                        {
+                            Console.Write(cc.Value[i] + ", ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
