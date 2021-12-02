7b21f1dac69d46d4f669e84631bb749b30fde4f4
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-12-02 23:52:16 +0200

    Add files via upload

---
+++ 4. SetsAndDictionaries-Advanced/6.ParkingLot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SetsAndDictionaries
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var parking = new HashSet<string>();
+            while(input != "END")
+            {
+                var inputParams = Regex.Split(input, ", ");
+                if(inputParams[0] == "IN")
+                {
+                    parking.Add(inputParams[1]);
+                }
+                else
+                {
+                    parking.Remove(inputParams[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if(parking.Count == 0)
+                Console.WriteLine("Parking Lot is Empty");
+            else
+            {
+                foreach (var p in parking)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+}
---
+++ 4. SetsAndDictionaries-Advanced/7.SoftUniParty/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionaries
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> codes = new HashSet<string>();
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (input == "PARTY")
+                    break;
+                codes.Add(input);
+            }
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                    break;
+                codes.Remove(input);
+            }
+            Console.WriteLine(codes.Count);
+            foreach(var code in codes)
+            {
+                if (Char.IsDigit(code[0]))
+                {
+                    Console.WriteLine(code);
+                    codes.Remove(code);
+                }
+            }
+            foreach(var code in codes)
+            {
+                Console.WriteLine(code);
+            }
+        }
+    }
+}
