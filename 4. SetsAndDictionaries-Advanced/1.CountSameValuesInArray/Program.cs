62f5272a76d5d868da91523dbb564a46b2947155
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-12-02 23:56:48 +0200

    Add files via upload

---
+++ 4. SetsAndDictionaries-Advanced/1.CountSameValuesInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionaries
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<double, int> keyValues = new Dictionary<double, int>();
+            string[] input = Console.ReadLine().Split();
+            for(int i = 0; i < input.Length; i++)
+            {
+                double number = double.Parse(input[i]);
+                if (keyValues.ContainsKey(number)) keyValues[number]++;
+                else keyValues.Add(number, 1);
+            }
+            foreach(var key in keyValues)
+            {
+                Console.WriteLine(key.Key + " - " + key.Value + " times");
+            }
+        }
+    }
+}
---
+++ 4. SetsAndDictionaries-Advanced/2.AverageStudentGrades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionaries
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+            int number = int.Parse(Console.ReadLine());
+            for(int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                if(students.ContainsKey(input[0]))
+                {
+                    students[input[0]].Add(decimal.Parse(input[1]));
+                }
+                else
+                {
+                    students.Add(input[0], new List<decimal>());
+                    students[input[0]].Add(decimal.Parse(input[1]));
+                }
+            }
+            foreach (var student in students)
+            {
+                decimal avg = 0;
+                Console.Write(student.Key + " -> ");
+                for(int i = 0; i < student.Value.Count; i++)
+                {
+                    avg += student.Value[i];
+                    Console.Write($"{student.Value[i]:f2} ");
+                }
+                avg /= student.Value.Count;
+                Console.WriteLine($"(avg: {avg:f2})");
+            }
+
+        }
+    }
+}
