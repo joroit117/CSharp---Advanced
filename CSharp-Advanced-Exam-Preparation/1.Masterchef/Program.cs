d973d32f225ec773c24fa9750e8f8d391610c519
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2022-02-10 10:26:29 +0200

    Add files via upload

---
+++ CSharp-Advanced-Exam-Preparation/1.Masterchef/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList<int> ingredientsValues = new LinkedList<int>();
+            LinkedList<int> freshnessLevelValues = new LinkedList<int>();
+            SortedDictionary<string, int> dishes = new SortedDictionary<string, int>();
+            string[] ingredients = Console.ReadLine().Split(' ');
+            for(int i = 0; i < ingredients.Length; i++)
+            {
+                int temp = int.Parse(ingredients[i]);
+                ingredientsValues.AddLast(temp);
+            }
+            string[] freshnessLevel = Console.ReadLine().Split(' ');
+            for(int j = 0; j < freshnessLevel.Length; j++)
+            {
+                int temp = int.Parse(freshnessLevel[j]);
+                freshnessLevelValues.AddLast(temp);
+            }
+            while(ingredientsValues.Count > 0 && freshnessLevelValues.Count > 0)
+            {
+                if (ingredientsValues.First() == 0)
+                    ingredientsValues.RemoveFirst();
+                long multiplicationResult = ingredientsValues.First() * freshnessLevelValues.Last();
+                switch (multiplicationResult)
+                {
+                    case 150:
+                        ingredientsValues.RemoveFirst();
+                        freshnessLevelValues.RemoveLast();
+                        if(dishes.ContainsKey("Dipping sauce"))
+                            dishes["Dipping sauce"]++;
+                        else
+                            dishes.Add("Dipping sauce", 1);
+                        break;
+                    case 250:
+                        ingredientsValues.RemoveFirst();
+                        freshnessLevelValues.RemoveLast();
+                        if (dishes.ContainsKey("Green salad"))
+                            dishes["Green salad"]++;
+                        else
+                            dishes.Add("Green salad", 1);
+                        break;
+                    case 300:
+                        ingredientsValues.RemoveFirst();
+                        freshnessLevelValues.RemoveLast();
+                        if (dishes.ContainsKey("Chocolate cake"))
+                            dishes["Chocolate cake"]++;
+                        else
+                            dishes.Add("Chocolate cake", 1);
+                        break;
+                    case 400:
+                        ingredientsValues.RemoveFirst();
+                        freshnessLevelValues.RemoveLast();
+                        if (dishes.ContainsKey("Lobster"))
+                            dishes["Lobster"]++;
+                        else
+                            dishes.Add("Lobster", 1);
+                        break;
+                    default:
+                        freshnessLevelValues.RemoveLast();
+                        ingredientsValues.AddFirst(ingredientsValues.Last() + 5);
+                        ingredientsValues.RemoveLast();
+                        break;
+                }
+            }
+            int ingredientsLeft = 0;
+            if (ingredientsValues.Count > 0)
+            {
+                while(ingredientsValues.Count > 0)
+                {
+                    ingredientsLeft += ingredientsValues.First();
+                    ingredientsValues.RemoveFirst();
+                }
+            }
+            if(dishes.Count >= 4)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+                if(ingredientsLeft > 0)
+                    Console.WriteLine($"Ingredients left: {ingredientsLeft}");
+                foreach(var dish in dishes)
+                {
+                    Console.WriteLine($"#{dish.Key} --> {dish.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+                if (ingredientsLeft > 0)
+                    Console.WriteLine($"Ingredients left: {ingredientsLeft}");
+                foreach (var dish in dishes)
+                {
+                    if(dish.Value > 0)
+                    {
+                        Console.WriteLine($"#{dish.Key} --> {dish.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced-Exam-Preparation/2.TheBattleOfTheFiveArmies/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ExamPreparation
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int armyArmor = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] field = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                var chars = Console.ReadLine().ToCharArray();
+                field[i] = chars;
+            }
+            bool throneReached = false;
+
+            int heroRow = 0;
+            int heroColumn = 0;
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < field[i].Length; j++)
+                {
+                    if (field[i][j] == 'A')
+                    {
+                        heroRow = i;
+                        heroColumn = j;
+                    }
+                }
+            }
+
+            while(!throneReached && armyArmor > 0)
+            {
+                int oldHeroRow = heroRow;
+                int oldHeroColumn = heroColumn;
+                string[] command = Console.ReadLine().Split(' ');
+                string direction = command[0];
+                int enemyRow = int.Parse(command[1]);
+                int enemyColumn = int.Parse(command[2]);
+                field[enemyRow][enemyColumn] = 'O';
+                switch (direction)
+                {
+                    case "up":
+                        heroRow--;
+                        break;
+                    case "down":
+                        heroRow++;
+                        break;
+                    case "right":
+                        heroColumn++;
+                        break;
+                    case "left":
+                        heroColumn--;
+                        break;
+                }
+                if (field[heroRow][heroColumn] == 'M')
+                {
+                    armyArmor--;
+                    throneReached = true;
+                    field[heroRow][heroColumn] = '-';
+                    field[oldHeroRow][oldHeroColumn] = '-';
+                    break;
+                }
+                if (heroRow < 0 || heroColumn < 0)
+                    armyArmor--;
+                else
+                {
+                    field[oldHeroRow][oldHeroColumn] = '-';
+                    armyArmor--;
+                    if (field[heroRow][heroColumn] == 'O')
+                        armyArmor -= 2;
+                    if (armyArmor > 0)
+                        field[heroRow][heroColumn] = 'A';
+                    else
+                        field[heroRow][heroColumn] = 'X';
+                }
+            }
+
+            if(throneReached)
+            {
+                Console.WriteLine($"The army managed to free the Middle World! Armor left: {armyArmor}");
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < field[i].Length; j++)
+                    {
+                        Console.Write(field[i][j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The army was defeated at {heroRow};{heroColumn}.");
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < field[i].Length; j++)
+                    {
+                        Console.Write(field[i][j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
